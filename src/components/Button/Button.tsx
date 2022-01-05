66b2901b3bded24dad22fe6cde6d3fa3bfa0d8b0
anjanisanu <anjanikrsanu@gmail.com>
2022-01-05 23:29:53 +0530

    Added button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import './Button.css'
 
 export interface ButtonProps {
   label: string;
+  variant: string;
+  style: object;
+  icon: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button className="button">{props.label}</button>;
+  return (
+    <button className={props.variant === 'primary' ? 'button' : 'button__delete'} style={props.style}>
+      {props.label}
+      <svg className='button__icon'>
+        <use xlinkHref={props.icon} />
+      </svg>
+    </button>
+  );
 };
 
 export default Button;
